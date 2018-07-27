f9754872430fcbb57da3ee29c126cf4cca783f28
Eliana K <ekirshe@gmail.com>
2018-07-27 02:44:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function iLoveTheBeatles(number){
   do {
     beatlesLove.push("I love the Beatles!"); number++;
   }
-  while (iLoveTheBeatles(number < 15));
+  while (number < 15);
   return beatlesLove;
 };
